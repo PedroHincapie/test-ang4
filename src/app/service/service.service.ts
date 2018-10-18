151feaf8ffd255f5e8bfc788e5b783db7787db03
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2018-10-18 06:52:29 -0500

    Crear informacion del servicio
    Se crea en detalle la informacion con la que el servicio trabajara

--- src/app/service/service.service.ts
+++ src/app/service/service.service.ts
@@ -4,6 +4,14 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ServiceService {
+  places = [
+    { id: 1, name: 'Las olas del mar'},
+    {id: 2, name: 'Los rios del abuela'}
+  ];
 
   constructor() { }
+
+  getPlaces() {
+    return this.places;
+  }
 }
