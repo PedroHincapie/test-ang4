410816279fda36bb011725091e363ba6ce54d959
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2018-10-18 07:20:43 -0500

    Creacion de logica del servicio

--- src/app/services/places.service.ts
+++ src/app/services/places.service.ts
@@ -4,6 +4,16 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class PlacesService {
+  places = [
+    {id: 1, name : 'La bomba del amparo'},
+    {id: 2, name : 'Las malas'},
+    {id: 3, name : 'Donde el sebas'},
+    {id: 4, name : 'La bomba'},
+  ];
 
   constructor() { }
+
+  getplaces() {
+    return this.places;
+  }
 }
